22db4a21ade73afb9b53f01bf0d75f3cf1d04731
Xi (Rossi) Luo <xi.rossi.luo@gmail.com>
2017-05-01 20:34:39 -0400

    new coverflow implementations

--- screenshot.js
+++ screenshot.js
@@ -3,13 +3,15 @@ var fs = require('fs');
 var args = system.args;
 
 var shotfolder = './img/talkScreenshots/';
-
+var baseurl = 'http://bigcomplexdata.com/slides/'; 
+// baseurl = 'file:///Users/xluo/Personal/Computing/MyGitHubWeb/rluo.github.io/slides/';
 
 var filename = shotfolder + args[2];
 console.log("In screenshot");
 
 var page = require('webpage').create();
-page.open('http://bigcomplexdata.com/slides/' + args[1], function() {
+page.settings.resourceTimeout = 50000;
+page.open( baseurl + args[1], function() {
 	setTimeout(function() {
 		page.render(shotfolder + args[2]);
 		console.log("Done screenshot: " + args[2]);
