8d8ddfb8cb006411bda4bfe26427369d16c885c6
Xi (Rossi) LUO <rossi.stat@gmail.com>
2019-12-18 23:17:37 -0600

    change screen shots by docker

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp');
-//var webshot = require('gulp-webshot');
-//var screenshot = require('url-to-screenshot');
-var fs = require('fs');
-var frontMatter = require('gulp-front-matter');
-//var gutil = require('gulp-util');
-//var transform = require('gulp-transform');
-var log = require('fancy-log');
-var exec = require('gulp-exec');
-var through = require('through2');
+const gulp = require('gulp');
+//const webshot = require('gulp-webshot');
+//const screenshot = require('url-to-screenshot');
+const fs = require('fs');
+const frontMatter = require('gulp-front-matter');
+//const gutil = require('gulp-util');
+//const transform = require('gulp-transform');
+const log = require('fancy-log');
+const exec = require('gulp-exec');
+const through = require('through2');
 
 fileExists = function() {
   return through.obj(function(file, encoding, callback) {
       var exists = fs.existsSync('./img/talkScreenshots/' + file.frontmatter.externallink + '.png');
       if (!exists) {
           log("Starting screenshot for " + file.frontmatter.previewimage);
-          var command = 'phantomjs screenshot.js ' + file.frontmatter.previewimage + ' ' + file.frontmatter.externallink + ".png";
+          var command = 'docker run --rm  -v   /Users/xluo/Personal/Computing/MyGitHubWeb/rluo.github.io:/mnt rossiluo/ubuntu bash -c  "/root/decktape/phantomjs /mnt/screenshot.js ' + file.frontmatter.previewimage + ' ' + file.frontmatter.externallink + '.png" ';
           file.command = command;
           callback(null, file);
       } else {
           log("Image already exists for " + file.frontmatter.previewimage);
-          file.command = 'echo haha';
+          file.command = 'echo "' + 'phantomjs screenshot.js ' + file.frontmatter.previewimage + ' ' + file.frontmatter.externallink + '.png "';
           callback(null, file);
       }
   });
@@ -33,6 +33,10 @@ gulp.task('screenshot', async function() {
         .on('data', function(data) {
             if (data.command !== '') {
                 log("Finished screenshot for " + data.frontmatter.previewimage);
+                log(data.command)
             }
         });
 });
+
+
+
--- screenshot.js
+++ screenshot.js
@@ -2,9 +2,12 @@ var system = require('system');
 var fs = require('fs');
 var args = system.args;
 
-var shotfolder = './img/talkScreenshots/';
-var baseurl = 'http://bigcomplexdata.com/slides/'; 
+// var shotfolder = './img/talkScreenshots/';
+var shotfolder = '/mnt/img/talkScreenshots/';
+// var baseurl = 'http://bigcomplexdata.com/slides/'; 
+var baseurl = 'file:///mnt/slides/'; // relative to this script 
 // baseurl = 'file:///Users/xluo/Personal/Computing/MyGitHubWeb/rluo.github.io/slides/';
+//# docker run --rm  -v   /Users/xluo/Personal/Computing/MyGitHubWeb/rluo.github.io:/mnt rossiluo/ubuntu bash -c "/root/decktape/phantomjs  /mnt/screenshot.js CAP_CMStat_2019.html#/7 CAP_CMStat_2019.html.png" 
 
 var filename = shotfolder + args[2];
 console.log("In screenshot");
@@ -18,3 +21,10 @@ page.open( baseurl + args[1], function() {
 		phantom.exit();
 	}, 120000);
 });
+
+// var donepng = fs.existsSync(shotfolder + args[2]);
+// if (donepng) {
+// 	console.log("Done screenshot: " + args[2]);
+// } else {
+// 	console.log("Failed screenshot: " + args[2]);
+// }
