fe098d6b1d24f2ce1b20d4d802bb18aa49a83470
rluo <xi.rossi.luo@gmail.com>
2021-09-21 11:26:28 -0500

    add  comparison

---
+++ screenshot2.js
@@ -0,0 +1,21 @@
+var system = require('system');
+var fs = require('fs');
+var args = system.args;
+
+var shotfolder = './img/talkScreenshots/';
+var baseurl = 'http://bigcomplexdata.com/slides/'; 
+// baseurl = 'file:///Users/xluo/Personal/Computing/MyGitHubWeb/rluo.github.io/slides/';
+
+var filename = shotfolder + args[2];
+console.log("In screenshot: " + baseurl + args[1]);
+
+var page = require('webpage').create();
+page.settings.resourceTimeout = 100000;
+page.open( baseurl + args[1], function(status) {
+	console.log(status)
+	setTimeout(function() {
+		page.render(shotfolder + args[2]);
+		console.log("Done screenshot: " + args[2]);
+		phantom.exit();
+	}, 120000);
+});
